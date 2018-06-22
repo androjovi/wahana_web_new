3434955792bc2db82504c04ee382ed599d13657a
androjovi <joviandro@outlook.com>
2018-06-22 16:21:31 +0700

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
   if (scroll >= 200) {
     $("nav").addClass("dark-nav");
     $(".nav-item a").css("color","white");
+    $(".fa").css("color","white")
     //$(".nav-item a").removeClass("nav-link");
   } else {
     $("nav").removeClass("dark-nav");
     $(".nav-item a").css("color","black");
+    $(".fa").css("color","black")
     $(".nav-item a").hover(
    function () {
      $(this).addClass("trun");
